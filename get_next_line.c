afbfa5137d4f55fa438c94185a3f98a984eaa558
Mathew Lucka <mlucka@3-D-6.42heilbronn.de>
2026-06-27 05:23:09 +0200

    extract und clean

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mlucka <mlucka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 13:16:41 by mlucka            #+#    #+#             */
-/*   Updated: 2026/06/27 00:30:38 by mlucka           ###   ########.fr       */
+/*   Updated: 2026/06/27 05:21:59 by mlucka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,35 @@ static char	*read_file(int fd, char *stash)
 	}
 	free(buffer);
 	return (stash);
+}
+
+static char	*extract_line(char *stash)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0;
+	j = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		i++;
+	}
+	line = malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	while (j <= i)
+	{
+		line[j] = stash[j];
+		j++;
+	}
+	line[j] = '\0';
+	return (line);
+}
+static char *clean_stash(char *stash)
+{
+	int i;
+	
 }
 
 char	*get_next_line(int fd)
